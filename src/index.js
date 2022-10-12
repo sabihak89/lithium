0aa43d05ba0d115afa1fdf5ccf5093fda3bd8e5c
Shiv-Raj-Singh <mt932747@gmail.com>
2022-10-12 18:38:41 +0530

    my first commit files from lithium

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-const express = require('express');
-var bodyParser = require('body-parser');
-
-const route = require('./routes/route.js');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/', route);
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
+function add_func(x,y){
+    return x+y
+}
+console.log(add_func(5,18))
